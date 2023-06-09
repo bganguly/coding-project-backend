f3afaca6820e96b19dff103d87613e61aba803f1
bganguly <gangulybikramjit@hotmail.com>
2023-06-09 01:52:53 -0700

    branch_03_backend - updating wip cors effort

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const app = express();
+var cors = require('cors')
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.render('index', { title: 'Express' });
 });
 
 app.use(express.static("public"));
+
 
 module.exports = router;
